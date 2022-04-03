195d39cc0250570d7ea3e6d902171a02cc6844a1
SaraLucasH <s.lucas.2016@alumnos.urjc.es>
2022-04-03 18:50:53 +0200

    SLH Update sieve of erastosthenes

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -1,17 +1,28 @@
 # Implementation of sieve of erastothenes algorithm in order to find all the prime numbers in a segment 1 -> n
 def sieve_of_eratosthenes(n:int)-> list:
     candidates=[]
-    result:set={}
-
+    result=set()
+    visited =set()
+    if(n<=1):
+        return result
     for i in range(2,n+1):
         candidates.append(i)
-
-    i=0 #Pointer to the next unmarked element   
+    
+    result.add(2)
+    i=0 #Pointer to the next unmarked element 
     while(i < len(candidates)):
-        for j in range(i, len(candidates)):
+        for j in range(i+1, len(candidates)):
             #Delete those elements multiples of i and greater than or equal to the square of i
-            if(j % i == 0 and j >= i^2):
+            if(candidates[j] % candidates[i] == 0 and candidates[j]  >= candidates[i]^2):
+                visited.add(candidates[j])
+
+                if candidates[j] in result :
+                    result.discard(candidates[j])
                 continue
-            result.add(j)
+
+            if candidates[j] not in visited:
+                result.add(candidates[j])        
         i+=1
     return result
+
+x= sieve_of_eratosthenes(10)
